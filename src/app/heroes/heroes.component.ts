72c6abf84edf452976763d9e9d2cecfe27f6ba72
Gabriel Nascimento de Oliveira <gndeoliveira@outlook.com>
2022-08-15 22:08:17 -0300

    Part 6: Simulate a data server

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,18 +10,15 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`${hero.name} clicked!`);
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
-  ngOnInit(): void {
+  getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
